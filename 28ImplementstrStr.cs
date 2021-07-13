98ba7c414cc0787d0e2a7a03296d4e7c34e98951
Александр Алексеевич Гаврилушкин <285855@niuitmo.ru>
2021-07-13 16:56:39 +0300

    28. ImplementStr (prefix_function)

---
+++ 28ImplementstrStr.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ */
+
+namespace Easy_Leetcode_Problems
+{
+    class ImplementstrStr
+    {
+        private int[] PrefixFunction(string s)
+        {
+            int[] pi = new int[s.Length];
+            pi[0] = 0;
+
+            for(int i = 1; i < s.Length; i++)
+            {
+                int k = pi[i - 1];
+
+                while(k > 0 && s[i] != s[k])
+                {
+                    k = pi[k - 1];
+                }
+
+                if(s[i] == s[k])
+                {
+                    k++;
+                }
+                pi[i] = k;
+            }
+            return pi;
+        }
+        public int StrStr(string haystack, string needle)
+        {
+            int[] pi = PrefixFunction(needle + "#" + haystack);
+
+            int n = needle.Length;
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (pi[n + i + 1] == n)
+                {
+                    return i - n + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
