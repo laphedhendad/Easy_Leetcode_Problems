34f88690129a95b0a899691e65ccd6530df78a7e
Александр Алексеевич Гаврилушкин <285855@niuitmo.ru>
2021-07-14 14:09:46 +0300

    35. SearchInsertPosition

---
+++ 35SearchInsertPosition.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+ * If not, return the index where it would be if it were inserted in order.
+ */
+namespace Easy_Leetcode_Problems
+{
+    class SearchInsertPosition
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            return SearchInsert(nums, target, 0, nums.Length - 1);
+        }
+
+        private int SearchInsert(int[] nums, int target, int left, int right)
+        {
+            if (left > right) return left;
+            int middle = left + (right - left) / 2;
+            if (nums[middle] == target) return middle;
+            else if (nums[middle] > target) return SearchInsert(nums, target, left, right - 1);
+            else if (nums[middle] < target) return SearchInsert(nums, target, left + 1, right);
+            else return middle;
+        }
+    }
+}
