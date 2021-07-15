9c0638d5b62fb32558a65b989ff97d25a608b1d3
Александр Алексеевич Гаврилушкин <285855@niuitmo.ru>
2021-07-15 11:33:43 +0300

    53. MaxSubArray

---
+++ 53MaxSubarray.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ */
+
+namespace Easy_Leetcode_Problems
+{
+    class MaxSubarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+
+            int maxSub = nums[0];
+            int curSum = 0;
+
+            foreach (int n in nums)
+            {
+                if (curSum < 0) curSum = 0;
+                curSum += n;
+                if (maxSub < curSum) maxSub = curSum;
+            }
+
+            return maxSub;
+
+        }
+    }
+}
