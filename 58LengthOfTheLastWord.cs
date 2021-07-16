c1310d74f55ef6eb2062ead8d83b85dbb1e40bf0
Александр Алексеевич Гаврилушкин <285855@niuitmo.ru>
2021-07-16 10:02:40 +0300

    Description for problem 58

--- 58LengthOfTheLastWord.cs
+++ 58LengthOfTheLastWord.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/*
+ * Given a string s consists of some words separated by spaces, return the length of the last word in the string.
+ * If the last word does not exist, return 0.
+ */
 
 namespace LengthOfLastWord
 {
