1d7cc4da47a517464cbd29515f74c49ae147b562
Александр Алексеевич Гаврилушкин <285855@niuitmo.ru>
2021-07-25 09:20:24 +0300

    Fast 69 Sqrt

---
+++ 69Sqrt.cs
@@ -0,0 +1,22 @@
+﻿/*
+* Given a non-negative integer x, compute and return the square root of x.
+* Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+*/
+using System;
+namespace Easy_Leetcode_Problems
+{
+    public class Solution
+    {
+        public int MySqrt(int x)
+        {
+            float eps = 0.01f, S = x, a = 1, b = x;
+            while (Math.Abs(a - b) > eps)
+            {
+                a = (a + b) / 2;
+                b = S / a;
+            }
+            if ((int)((a + b) / 2) * (int)((a + b) / 2) > x) return (int)((a + b - 1) / 2);
+            return (int)((a + b) / 2);
+        }
+    }
+}
